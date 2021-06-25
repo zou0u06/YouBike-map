a6b1823be985edc142f9acf6ba173dd33f0ea2ff
zou0u06 <zou0u06@yahoo.com.tw>
2021-06-26 01:19:23 +0800

    feat:完成marker及popup顯示機制、加入分層顯示機制

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,63 @@
 <template>
   <div class="container-fluid">
-    <div class="row no-gutters">
-      <div class="menubox col-md-3">
-        <div class="shadow-sm p-2">
-          <div class="form-group d-flex">
-            <label for="area" class="mr-2 col-form-label">地區</label>
-            <div class="flex-fill menubox-bar">
-              <select id="area" class="form-control" v-model="location">
-                <option value>-- 請選擇要查詢的行政區 --</option>
-                <option value="中山區">中山區</option>
-                <option value="中正區">中正區</option>
-              </select>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="searchbox">
+          <div class="sticky-top">
+            <div class="searchbox-bar input-group d-flex">
+              <input type="text" class="form-control w-100" v-model="searchText">
             </div>
           </div>
-          <p class="mb-0 small text-muted text-right">請先選擇區域查看（綠色表示還有車輛）</p>
+          <ul
+            class="list-group"
+          >
+            <li
+              v-for="(display, index) in displays"
+              :key="display.sno"
+              class="list-group-item"
+              @click="focusOn(display, index)"
+            >
+              <h4>{{ display.sna }}</h4>
+              <h6>總停車格數量：{{ display.tot }}</h6>
+              <h6>可借車輛數：{{ display.sbi }}</h6>
+              <h6>可還車位數：{{ display.bemp }}</h6>
+              <h6>資料更新時間：{{ display.mday }}</h6>
+              <h6>
+                地址：
+                <a
+                  href="https://www.google.com.tw/maps/place/..."
+                  target="_blank"
+                  title="Google Map"
+                >地址</a>
+              </h6>
+            </li>
+          </ul>
         </div>
-        <ul class="list-group" v-for="youbike in youbikes" :key="youbike.sno">
-          <a class="list-group-item text-left">
-            <h4>{{ youbike.sna }}</h4>
-            <h6>總停車格數量：{{ youbike.tot }}</h6>
-            <h6>可借車輛數：{{ youbike.sbi }}</h6>
-            <h6>可還車位數：{{ youbike.bemp }}</h6>
-            <h6>資料更新時間：{{ youbike.mday }}</h6>
-            <p class="mb-0">
-              地址：
-              <a
-                href="https://www.google.com.tw/maps/place/..."
-                target="_blank"
-                title="Google Map"
-              >地址</a>
-            </p>
-          </a>
-        </ul>
       </div>
-      <div class="col-sm-9">
-        <l-map :zoom="zoom" :center="center">
-          <l-tile-layer :url="url" />
-          <l-marker
-            :lat-lng="[youbike.lat, youbike.lng]"
-            v-for="youbike in youbikes"
-            :key="youbike.sno"
-          />
+      <div class="col-md-9 map">
+        <l-map
+          :zoom="zoom"
+          :center="center"
+          :options="{zoomControl: false}"
+        >
+          <l-tile-layer :url="url"/>
+          <l-control-zoom position="bottomleft"/>
+          <v-marker-cluster>
+            <l-marker
+              ref="marker"
+              v-for="display in displays"
+              :key="display.sno"
+              :lat-lng="[display.lat, display.lng]"
+            >
+              <l-popup>
+                <h4>{{ display.sna }}</h4>
+                <h6>總停車格數量：{{ display.tot }}</h6>
+                <h6>可借車輛數：{{ display.sbi }}</h6>
+                <h6>可還車位數：{{ display.bemp }}</h6>
+                <h6>資料更新時間：{{ display.mday }}</h6>
+              </l-popup>
+            </l-marker>
+          </v-marker-cluster>
         </l-map>
       </div>
     </div>
@@ -48,62 +65,43 @@
 </template>
 
 <script>
-// eslint-disable-next-line no-unused-vars
-// import L from 'leaflet';
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
-
-console.log('top');
-
+/* eslint-disable no-restricted-syntax */
 export default {
   name: 'App',
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-  },
   data() {
     return {
-      youbikes: {},
+      youbikes: {
+        '0001': {
+          lat: 0,
+          lng: 0,
+        },
+      },
       weather: [],
-      location: '',
+      searchText: '',
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       zoom: 17,
-      center: [25.047845898630204, 121.5168620095192],
+      center: [25.047739238212436, 121.5168348654844],
     };
   },
-  methods: {
-    getYoubikes() {
-      console.log('getYoubike');
-
-      // this.markers = L.markerClusterGroup().addTo(OSM);
-      // console.log(this.markers);
-
-      const youbikeAPI = 'https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json';
-      this.axios.get(youbikeAPI).then((response) => {
-        console.log(response);
-        if (response) {
-          this.youbikes = response.data.retVal;
-          console.log(this.youbikes);
-        } else {
-          console.log('伺服器錯誤，請稍後再試');
-        }
-      });
-    },
-    getWeather() {
-      const weatherAPI = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-069?Authorization=rdec-key-123-45678-011121314';
-      this.axios.get(weatherAPI).then((response) => {
-        if (response) {
-          this.weather = response.data.records.locations[0].location;
-          // console.log(this.weather);
-        } else {
-          console.log('天氣API錯誤');
-        }
-      });
-    },
-  },
   computed: {
-    display() {
-      console.log('display');
+    displays() {
+      const temp = [];
+      switch (this.searchText) {
+        case '': {
+          for (const key in this.youbikes) {
+            if ({}.hasOwnProperty.call(this.youbikes, key)) {
+              temp.push(this.youbikes[key]);
+            }
+          }
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+      console.log(temp);
+      return temp;
+      // return this.youbikes.filter((item) => item.ar.includes(searchText));
       // this.weather.filter((item) => item.locationName === this.location);
       // switch (this.location) {
       //   case '':
@@ -115,36 +113,84 @@ export default {
       //     return tempData;
       //   }
       // }
-      return false;
+    },
+  },
+  methods: {
+    getYoubikes() {
+      const youbikeAPI = 'https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json';
+      this.axios.get(youbikeAPI).then((response) => {
+        console.log(response);
+        if (response) {
+          this.youbikes = response.data.retVal;
+          console.log(this.youbikes);
+        } else {
+          console.log('伺服器錯誤，請稍後再試');
+        }
+      });
+    },
+    // getWeather() {
+    //   const weatherAPI = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-069?Authorization=rdec-key-123-45678-011121314';
+    //   this.axios.get(weatherAPI).then((response) => {
+    //     if (response) {
+    //       this.weather = response.data.records.locations[0].location;
+    //       // console.log(this.weather);
+    //     } else {
+    //       console.log('天氣API錯誤');
+    //     }
+    //   });
+    // },
+    focusOn(display, index) {
+      if (document.body.clientWidth < 767.98) {
+        const tempLat = String(Number(display.lat) + 0.002);
+        this.center = [tempLat, display.lng];
+      } else {
+        this.center = [display.lat, display.lng];
+      }
+      this.$nextTick(() => {
+        this.$refs.marker[index].mapObject.openPopup();
+      });
     },
   },
   mounted() {
     this.getYoubikes();
-    this.getWeather();
+    // this.getWeather();
+    // const vm = this;
+    // console.log(vm.$refs.marker.mapObject);
+    // vm.$nextTick(() => {
+    //   vm.$refs.marker.mapObject.openPopup();
+    // });
   },
 };
 </script>
 
 <style lang="scss">
-@import "bootstrap/scss/bootstrap";
+@import "./assets/scss/all.scss";
 
-#mapId {
-  height: 100%;
+.map {
+  height: 100vh;
 }
 .highlight {
   background: #e9ffe3;
 }
-.menubox {
-  height: 60%;
+.searchbox {
+  height: 30%;
   overflow-y: auto;
   position: fixed;
   top: 30px;
+  z-index: 401;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+  border-radius: 0.25rem;
   background-color: #fff;
   @include media-breakpoint-up(md) {
-    height: 100%;
+    position: static;
+    margin-left: 0;
+    margin-right: 0;
+    padding: 0.5rem;
+    height: 100vh;
   }
 }
-.menubox-bar {
+.searchbox-bar {
   height: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+/* eslint-disable no-underscore-dangle */
 import Vue from 'vue';
 import axios from 'axios';
 import vueAxios from 'vue-axios';
 import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+import {
+  LMap, LTileLayer, LMarker, LControlZoom, LPopup,
+} from 'vue2-leaflet';
+import Vue2LeafletMarkerCluster from 'vue2-leaflet-markercluster';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
 import App from './App.vue';
+
+// 處理icon消失的程式碼
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+Vue.component('l-control-zoom', LControlZoom);
+Vue.component('l-popup', LPopup);
+Vue.component('v-marker-cluster', Vue2LeafletMarkerCluster);
 
 Vue.config.productionTip = false;
 
