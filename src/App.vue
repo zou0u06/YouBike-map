b2abdd2210377af47a7e73a4f7b25b3c67ea42f7
zou0u06 <zou0u06@yahoo.com.tw>
2021-07-05 23:26:14 +0800

    refactor&style:以 compo 減少 HTML tag 數量、調 sidebar 樣式

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,86 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-4">
-        <div class="featurebox">
-          <div class="featurebox-bar">
-            <input
-              type="text"
-              v-model="searchText"
-              placeholder="尋找站點"
-              class="featurebox-bar-input"
-            />
-            <span
-              class="featurebox-bar-icon"
-              @click="navsActive = 'searchs'"
-            >
-              <i class="fas fa-search"/>
-            </span>
-            <span
-              class="featurebox-bar-icon"
-              @click="navsActive = 'favs'"
-            >
-              <i class="fas fa-star"/>
-            </span>
-          </div>
-          <ul
-            v-if="navsActive === 'searchs'"
-            class="list-group featurebox-result"
-          >
-            <li
-              v-for="search in searchs"
-              :key="search.sno"
-              class="list-group-item"
-              :class="{ 'text-danger': search.act === '0'}"
-              @click="focusOn(search)"
-            >
-              <h4 class="d-flex justify-content-between align-items-center">
-                <span>{{ search.sna }}</span>
-                <i
-                  v-if="!search.favored"
-                  class="icon-favs far fa-star"
-                  @click.stop="addToFavYoubikes(search.sno)"
-                />
-                <i
-                  v-else
-                  class="icon-favs fas fa-star"
-                  @click.stop="deletedFavYoubikes(search.sno)"
-                />
-              </h4>
-              <h6 class="d-flex justify-content-between">
-                <span v-if="search.act === '1'">正常營運</span>
-                <span v-else>暫停營運</span>
-                <span>可借：{{ search.sbi }}</span>
-                <span>可還：{{ search.bemp }}</span>
-              </h6>
-              <h6 class="mb-0">地址：{{ search.ar }}</h6>
-            </li>
-          </ul>
-          <ul
-            v-if="navsActive === 'favs'"
-            class="list-group featurebox-result"
-          >
-            <li
-              v-if="favs.length === 0"
-              class="mt-3 text-center"
-            >
-              目前尚無儲存站點
-            </li>
-            <li
-              v-for="fav in favs"
-              :key="fav.sno"
-              class="list-group-item"
-              :class="{ 'text-danger': fav.act === '0'}"
-              @click="focusOn(fav)"
-            >
-              <h4 class="d-flex justify-content-between align-items-center">
-                <span>{{ fav.sna }}</span>
-                <i
-                  v-if="!fav.favored"
-                  class="icon-favs far fa-star"
-                  @click.stop="addToFavYoubikes(fav.sno)"
-                />
-                <i
-                  v-else
-                  class="icon-favs fas fa-star"
-                  @click.stop="deletedFavYoubikes(fav.sno)"
-                />
-              </h4>
-              <h6 class="d-flex justify-content-between">
-                <span v-if="fav.act === '1'">正常營運</span>
-                <span v-else>暫停營運</span>
-                <span>可借：{{ fav.sbi }}</span>
-                <span>可還：{{ fav.bemp }}</span>
-              </h6>
-              <h6 class="mb-0">地址：{{ fav.ar }}</h6>
-            </li>
-          </ul>
+      <button
+        type="button"
+        class="btn-sidebar"
+        :class="{ 'd-none': sidebarActive, 'd-block': !sidebarActive}"
+        @click="sidebarActive = !sidebarActive"
+      >
+        快點我
+      </button>
+      <div
+        class="col-md-4 sidebar"
+        :class="{ 'd-none': !sidebarActive, 'd-block': sidebarActive}"
+      >
+        <div class="sidebar-header">
+          <button
+            type="button"
+            class="btn-close"
+            aria-label="Close"
+            @click="sidebarActive = false"
+          />
+          <input
+            type="text"
+            v-model="searchText"
+            placeholder="尋找站點"
+            class="sidebar-input"
+          />
+        </div>
+        <ul class="nav sidebar-nav">
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              @click="switchNavsActive('searchs')"
+              :class="{ 'text-secondary': navsActive === 'searchs'}"
+            >搜尋結果</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              @click="switchNavsActive('favs')"
+              :class="{ 'text-secondary': navsActive === 'favs'}"
+            >收藏站點</a>
+          </li>
+        </ul>
+        <div
+          v-if="navsActive === 'searchs'"
+          class="sidebar-result"
+        >
+          <h6
+            v-if="searchs.length === 0 || !searchs"
+            class="mt-3 text-center"
+          >查無相關站點</h6>
+          <card-result
+            v-else
+            :bases="searchs"
+            @changeNavsActive="closeSidebar()"
+            @deleteFavYoubikes="deleteFavYoubikes"
+            @addToFavYoubikes="addToFavYoubikes"
+          />
+        </div>
+        <div
+          v-if="navsActive === 'favs'"
+          class="sidebar-result"
+        >
+          <h6
+            v-if="favs.length === 0 || !favs"
+            class="mt-3 text-center"
+          >目前尚無收藏站點</h6>
+          <card-result
+            v-else
+            :bases="favs"
+            @changeNavsActive="closeSidebar()"
+            @deleteFavYoubikes="deleteFavYoubikes"
+            @addToFavYoubikes="addToFavYoubikes"
+          />
         </div>
       </div>
-      <div class="col-md-8 map">
+      <div
+        class="map"
+        :class="{ 'col': sidebarActive, 'col-md-8': sidebarActive}"
+        @click="closeSidebar()"
+      >
         <l-map ref="map" :zoom="zoom" :center="center" :options="{zoomControl: false}">
           <l-tile-layer :url="url" />
           <l-control-zoom position="bottomright" />
@@ -108,30 +91,39 @@
               :key="youbike.sno"
               :lat-lng="[youbike.lat, youbike.lng]"
             >
-              <l-popup :class="{ 'text-danger': youbike.act === '0'}">
-                <h4 class="d-flex justify-content-between align-items-center">
-                  <span>{{ youbike.sna }}</span>
+              <l-popup class="position-relative">
+                <div class="d-flex justify-content-between">
+                  <div class="flex-fill">
+                    <h4 class="text-primary bg-white">
+                      {{ youbike.sna }}
+                      <span v-if="youbike.act === '0'">（暫停營運）</span>
+                    </h4>
+                    <h6>溫度：{{ youbike.temperature }}°C</h6>
+                  </div>
                   <i
-                    v-if="!youbike.favored"
-                    class="icon-popup icon-favs far fa-star"
-                    @click.stop="addToFavYoubikes(youbike.sno)"
+                    v-if="youbike.favored"
+                    class="fas fa-star popup-icon-favs"
+                    @click.stop="deleteFavYoubikes(youbike.sno)"
                   />
                   <i
                     v-else
-                    class="icon-popup icon-favs fas fa-star"
-                    @click.stop="deletedFavYoubikes(youbike.sno)"
+                    class="far fa-star popup-icon-favs"
+                    @click.stop="addToFavYoubikes(youbike.sno)"
                   />
-                </h4>
-                <h6 class="d-flex justify-content-between">
-                  <span v-if="youbike.act === '1'">正常營運</span>
-                  <span v-else>暫停營運</span>
-                  <span>可借：{{ youbike.sbi }}</span>
-                  <span>可還：{{ youbike.bemp }}</span>
-                </h6>
-                <h6>地址：{{ youbike.ar }}</h6>
-                <h6>近三小時內天氣：{{ youbike.weather }}</h6>
-                <h6>近三小時內平均溫度：{{ youbike.temperature }}°C</h6>
+                </div>
+                <h6>天氣：{{ youbike.weather }}</h6>
+                <h6>位置：{{ youbike.ar }}</h6>
                 <h6>資料更新時間  {{ youbike.mday|displayDate }}</h6>
+                <div class="d-flex">
+                  <h6
+                    class="popup-footer-block1"
+                    :class="{'bg-success': youbike.sbi > 0, 'bg-danger': youbike.sbi === '0'}"
+                  >可借：{{ youbike.sbi }}</h6>
+                  <h6
+                    class="popup-footer-block2"
+                    :class="{'bg-success': youbike.sbi > 0, 'bg-danger': youbike.sbi === '0'}"
+                  >可還：{{ youbike.bemp }}</h6>
+                </div>
               </l-popup>
             </l-marker>
           </v-marker-cluster>
@@ -142,8 +134,11 @@
 </template>
 
 <script>
+import cardResult from './components/CardResult.vue';
+
 export default {
   name: 'App',
+  components: { cardResult },
   data() {
     return {
       youbikes: [
@@ -156,39 +151,41 @@ export default {
       weathers: [],
       searchText: '',
       navsActive: '',
+      sidebarActive: false,
+      bases: [],
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       zoom: 13,
-      center: [25.047739238212436, 121.5168348654844],
+      center: [25.049, 121.515],
     };
   },
   computed: {
     searchs() {
+      const tempSearchs = [];
+      const youbikesL = this.youbikes.length;
       switch (this.searchText) {
-        case '': {
-          return false;
-        }
-        default: {
-          const tempSearchs = [];
-          this.youbikes.forEach((item, oriIndex) => {
+        case '':
+          return '';
+        default:
+          for (let i = 0; i < youbikesL; i++) {
             if (
-              item.ar.indexOf(this.searchText) > -1
-              || item.sna.indexOf(this.searchText) > -1
-              || item.sarea.indexOf(this.searchText) > -1
+              this.youbikes[i].ar.indexOf(this.searchText) > -1
+              || this.youbikes[i].sna.indexOf(this.searchText) > -1
+              || this.youbikes[i].sarea.indexOf(this.searchText) > -1
             ) {
-              tempSearchs.push({ ...item, oriIndex });
+              tempSearchs.push({ ...this.youbikes[i], oriIndex: i });
             }
-          });
+          }
           return tempSearchs;
-        }
       }
     },
     favs() {
       const tempFavs = [];
-      this.youbikes.forEach((item, oriIndex) => {
-        if (item.favored) {
-          tempFavs.push({ ...item, oriIndex });
+      const youbikesL = this.youbikes.length;
+      for (let i = 0; i < youbikesL; i++) {
+        if (this.youbikes[i].favored) {
+          tempFavs.push({ ...this.youbikes[i], oriIndex: i });
         }
-      });
+      }
       return tempFavs;
     },
   },
@@ -198,8 +195,8 @@ export default {
         this.getWeathers();
       }
     },
-    searchText(searchText) {
-      switch (searchText) {
+    searchText() {
+      switch (this.searchText) {
         case '':
           this.navsActive = '';
           break;
@@ -208,6 +205,11 @@ export default {
           break;
       }
     },
+    // navsActive() {
+    //   switch (this.navsActive) {
+    //     case ''
+    //   }
+    // }
   },
   methods: {
     getYoubikes() {
@@ -253,7 +255,7 @@ export default {
         }
       }
     },
-    deletedFavYoubikes(deletedId) {
+    deleteFavYoubikes(deletedId) {
       const favYoubikesId = JSON.parse(localStorage.getItem('favYoubikesId')) || [];
       const filteredId = favYoubikesId.filter((item) => item !== deletedId);
       localStorage.setItem('favYoubikesId', JSON.stringify(filteredId));
@@ -277,108 +279,53 @@ export default {
       });
     },
     setWeathers() {
-      this.youbikes.forEach((item, index) => {
-        this.weathers.forEach((ite) => {
+      const youbikesL = this.youbikes.length;
+      const weathersL = this.weathers.length;
+      for (let i = 0; i < youbikesL; i++) {
+        for (let j = 0; j < weathersL; j++) {
           if (
-            item.sarea === ite.locationName
+            this.youbikes[i].sarea === this.weathers[j].locationName
             && new Date().getHours() % 6 < 3
           ) {
-            this.$set(this.youbikes[index], 'weather', ite.weatherElement[1].time[0].elementValue[0].value);
-            this.$set(this.youbikes[index], 'temperature', ite.weatherElement[3].time[0].elementValue[0].value);
+            this.$set(this.youbikes[i], 'weather', this.weathers[j].weatherElement[1].time[0].elementValue[0].value);
+            this.$set(this.youbikes[i], 'temperature', this.weathers[j].weatherElement[3].time[0].elementValue[0].value);
           } else if (
-            item.sarea === ite.locationName
+            this.youbikes[i].sarea === this.weathers[j].locationName
             && new Date().getHours() % 6 >= 3
           ) {
-            this.$set(this.youbikes[index], 'weather', ite.weatherElement[1].time[1].elementValue[0].value);
-            this.$set(this.youbikes[index], 'temperature', ite.weatherElement[3].time[1].elementValue[0].value);
+            this.$set(this.youbikes[i], 'weather', this.weathers[j].weatherElement[1].time[1].elementValue[0].value);
+            this.$set(this.youbikes[i], 'temperature', this.weathers[j].weatherElement[3].time[1].elementValue[0].value);
           }
-        });
-      });
-    },
-    focusOn(display) {
-      if (document.body.clientWidth < 767.98) {
-        const tempLat = String(Number(display.lat) + 0.0021);
-        this.$refs.map.mapObject.setView([tempLat, display.lng], 17);
-      } else {
-        this.$refs.map.mapObject.setView([display.lat, display.lng], 17);
+        }
       }
-      this.$nextTick(() => {
-        this.$refs.marker[display.oriIndex].mapObject.openPopup();
-      });
+    },
+    closeSidebar() {
+      if (document.body.clientWidth < 767.98) {
+        this.sidebarActive = false;
+      }
+    },
+    switchNavsActive(keyword) {
+      switch (this.navsActive) {
+        case keyword:
+          this.navsActive = '';
+          break;
+        default:
+          this.navsActive = keyword;
+          break;
+      }
     },
   },
   created() {
     this.getYoubikes();
+    if (document.body.clientWidth < 767.98) {
+      this.sidebarActive = false;
+    } else {
+      this.sidebarActive = true;
+    }
   },
 };
 </script>
 
 <style lang="scss">
 @import "./assets/scss/all.scss";
-
-.map {
-  height: 100vh;
-}
-.featurebox {
-  position: fixed;
-  top: 30px;
-  left: 10px;
-  right: 10px;
-  z-index: 401;
-  border-radius: 0.25rem;
-  background-color: #fff;
-  @include media-breakpoint-up(md) {
-    position: static;
-    margin-left: 0;
-    margin-right: 0;
-    padding: 0.5rem;
-    height: 100vh;
-  }
-}
-.featurebox-bar {
-  height: 60px;
-  display: flex;
-  justify-content: end;
-  border: 1px solid #ced4da;
-  border-radius: 0.25rem;
-  line-height: 1.5;
-  font-weight: 400;
-  font-size: 1rem;
-  color: #212529;
-}
-.featurebox-bar-input {
-  flex: 1 1 auto;
-  min-width: 140px;
-  border: none;
-  padding: 0.375rem 0.75rem;
-}
-.featurebox-bar-icon {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-width: 44px;
-  text-align: center;
-  cursor: pointer;
-}
-.featurebox-result {
-  height: 25vh;
-  overflow-y: auto;
-  cursor: pointer;
-  @include media-breakpoint-up(md) {
-    height: calc(100vh - 68px);
-  }
-}
-.icon-favs {
-  font-size: 20px;
-}
-.icon-popup {
-  position: relative;
-  bottom: 2px;
-}
-.leaflet-popup-content {
-  width: 260px !important;
-}
-.leaflet-popup-close-button {
-  display: none;
-}
 </style>
---
+++ src/components/CardResult.vue
@@ -0,0 +1,60 @@
+<template>
+  <div style="overflow-y: auto">
+    <div
+      v-for="base in bases"
+      :key="base.sno"
+      class="cardresult"
+      @click="focusOn(base)"
+    >
+      <div class="d-flex">
+        <h5 class="flex-fill text-primary">
+          {{ base.sna }}
+          <span v-if="base.act === '0'">（暫停營運）</span>
+        </h5>
+        <i
+          v-if="base.favored"
+          class="cardresult-icon-favs fas fa-star"
+          @click.stop="deleteFavYoubikes(base.sno)"
+        />
+        <i
+          v-else
+          class="cardresult-icon-favs far fa-star"
+          @click.stop="addToFavYoubikes(base.sno)"
+        />
+      </div>
+      <h6 class="mb-0">{{ base.ar }}</h6>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bases: Array,
+  },
+  methods: {
+    focusOn(base) {
+      if (document.body.clientWidth < 767.98) {
+        const tempLat = String(Number(base.lat) + 0.0013);
+        this.$parent.$refs.map.mapObject.setView([tempLat, base.lng], 17);
+      } else {
+        this.$parent.$refs.map.mapObject.setView([base.lat, base.lng], 17);
+      }
+      this.$nextTick(() => {
+        this.$parent.$refs.marker[base.oriIndex].mapObject.openPopup();
+      });
+      this.$emit('changeNavsActive', '');
+    },
+    deleteFavYoubikes(BaseId) {
+      this.$emit('deleteFavYoubikes', BaseId);
+    },
+    addToFavYoubikes(BaseId) {
+      this.$emit('addToFavYoubikes', BaseId);
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
